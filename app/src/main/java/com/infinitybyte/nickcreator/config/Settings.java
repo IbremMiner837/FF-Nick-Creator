88697b53fa7821d73d16038af06df199aaa4981e
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-02-10 21:38:32 +0100

    - Added the ability to select a random nickname - Added settings - Slightly changed design - Added warning that you need to enter a nickname - Added English and Russian strings - Added Chechen, Russian and random nicknames

---
+++ app/src/main/java/com/infinitybyte/nickcreator/config/Settings.java
@@ -0,0 +1,6 @@
+package com.infinitybyte.nickcreator.config;
+
+public class Settings {
+    @Parameter(jsonKey = "nickname_array")
+    public static String nicknamesArray = "chechen_males_nicknames";
+}
