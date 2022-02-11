8e8edeed07274e35b9454de47f61b3cc36f807bd
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-02-11 22:30:27 +0100

    - New desing for font selection screen - Added new strings

--- app/src/main/java/com/infinitybyte/nickcreator/SelectFontActivity.java
+++ app/src/main/java/com/infinitybyte/nickcreator/SelectFontActivity.java
@@ -62,7 +62,7 @@ public class SelectFontActivity extends AppCompatActivity {
         before_step.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                finish();
             }
         });
 
